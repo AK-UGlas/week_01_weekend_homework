f65d9671e334530f990eb06c3f524bbe6e22fdaf
Allen_K <allenkelly278@gmail.com>
2021-04-04 09:13:15 +0100

    passing tests 4-7

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -11,3 +11,22 @@ def get_total_cash(pet_shop_data):
 # 3: add or remove cash from total
 def add_or_remove_cash(pet_shop_data, amount):
     pet_shop_data["admin"]["total_cash"] += amount
+
+# 4: get the number of animals sold
+def get_pets_sold(pet_shop_data):
+    return pet_shop_data["admin"]["pets_sold"]
+
+# 5: increase the number of pets sold
+def increase_pets_sold(pet_shop_data, num_pets_sold):
+    pet_shop_data["admin"]["pets_sold"] += num_pets_sold
+
+# 6: get stock count
+def get_stock_count(pet_shop_data):
+    return len(pet_shop_data["pets"])
+
+# 7: get pets by breed
+def get_pets_by_breed(pet_shop_data, breed):
+    return [pet for pet in pet_shop_data["pets"] 
+            if pet["breed"] == breed]
+
+# 8: 
