073c0eb81d78bdfba10b64f918cca148c373decf
Allen_K <allenkelly278@gmail.com>
2021-04-04 08:31:11 +0100

    passing tests 1-3

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,1 +1,13 @@
-# WRITE YOUR FUNCTIONS HERE
+# functions to run a pet shop
+
+# 1: get name of shop
+def get_pet_shop_name(pet_shop_data):
+    return pet_shop_data["name"]
+
+# 2: get pet shop total cash
+def get_total_cash(pet_shop_data):
+    return pet_shop_data["admin"]["total_cash"]
+
+# 3: add or remove cash from total
+def add_or_remove_cash(pet_shop_data, amount):
+    pet_shop_data["admin"]["total_cash"] += amount
